32545af5e1f9f5d8c708f973b850f2c86049cde7
duubyPlz <duubyplz@hotmail.com>
2020-07-05 19:53:20 +1000

    auto update

---
+++ save.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from subprocess import Popen, PIPE
+
+if __name__ == "__main__":
+    derp = "尸";
+    p1 = Popen(["echo",'"{}"'.format(derp)], stdout=PIPE)
+    p2 = Popen(["iconv", "-f utf-8 -t big5"], stdin=p1.stdout, stdout=PIPE)
+    p1.stdout.close()
+
+    output, errors = p2.communicate()
+
+    print("OUTPUT")
+    print(output)
+    print("ERRORS")
+    print(errors)
+#echo "尸" | iconv -f utf-8 -t 
